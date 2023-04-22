1b0eac0ae8911ed74af44c7ee0f50bbbcc70b52d
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2023-04-22 14:18:22 -0500

    Add User Class and test

---
+++ src/main/java/com/docseeker/backend/controller/User.java
@@ -0,0 +1,67 @@
+package com.docseeker.backend.controller;
+
+public class User {
+    private String id;
+    private String userType;
+    private String email;
+    private String password;
+    private String dni;
+    private int age;
+
+    public User(String id, String userType, String email, String password, String dni, int age) {
+        this.id = id;
+        this.userType = userType;
+        this.email = email;
+        this.password = password;
+        this.dni = dni;
+        this.age = age;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ src/test/java/com/docseeker/backend/model/UserTest.java
@@ -0,0 +1,24 @@
+package com.docseeker.backend.model;
+
+import com.docseeker.backend.controller.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+class UserTest {
+    @Test
+    void create_new_user_patient() {
+        User user = new User(
+                UUID.randomUUID().toString(),
+                "patient",
+                "patient_test@gmail.com",
+                "testpassword",
+                "75730173",
+                23
+        );
+
+        Assertions.assertNotNull(user);
+        Assertions.assertEquals("patient", user.getUserType());
+    }
+}
