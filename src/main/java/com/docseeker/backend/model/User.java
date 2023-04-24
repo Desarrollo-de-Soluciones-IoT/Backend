300cf8dea726fb9d8e4481fa6c2aa1188a0d466f
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2023-04-23 20:48:27 -0500

    Add Validation to User class

--- src/main/java/com/docseeker/backend/model/User.java
+++ src/main/java/com/docseeker/backend/model/User.java
@@ -1,8 +1,13 @@
 package com.docseeker.backend.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+
 public record User(
         String id,
+        @NotEmpty
         UserType userType,
+        @Email
         String email,
         String password,
         String dni,
