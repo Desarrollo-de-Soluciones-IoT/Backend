70c61a6ab329844d88ecde307db1b2214144ce01
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2023-04-25 17:21:15 -0500

    Make User a superclass

--- src/main/java/com/docseeker/backend/model/User.java
+++ src/main/java/com/docseeker/backend/model/User.java
@@ -1,16 +1,22 @@
 package com.docseeker.backend.model;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
-public record User(
-        String id,
-        @NotEmpty
-        UserType userType,
-        @Email
-        String email,
-        String password,
-        String dni,
-        int age
-) {
+@MappedSuperclass
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private int id;
+        private UserType userType;
+        private String email;
+        private String password;
+        private String dni;
+        private int age;
 }
