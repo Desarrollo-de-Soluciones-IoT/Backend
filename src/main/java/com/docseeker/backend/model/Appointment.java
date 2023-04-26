dda61c2a1eedfd237b12c634fee810827be82bee
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2023-04-26 10:49:24 -0500

    Add Getter and Setter annotations

--- src/main/java/com/docseeker/backend/model/Appointment.java
+++ src/main/java/com/docseeker/backend/model/Appointment.java
@@ -2,7 +2,9 @@ package com.docseeker.backend.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
@@ -10,6 +12,8 @@ import java.time.LocalDateTime;
 @Table(name = "appointments")
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
 public class Appointment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
