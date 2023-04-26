782255bc8bfaf0ec0578115e52aa976c16eb1b9d
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2023-04-26 07:37:48 -0500

    Add relationship with Appointment class

--- src/main/java/com/docseeker/backend/model/Doctor.java
+++ src/main/java/com/docseeker/backend/model/Doctor.java
@@ -1,6 +1,7 @@
 package com.docseeker.backend.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
@@ -12,6 +13,8 @@ import lombok.NoArgsConstructor;
 public class Doctor extends User {
     private String speciality;
     private int experienceYears;
+    @OneToOne(mappedBy = "doctor")
+    private Appointment appointment;
 
     public Doctor(int id, UserType userType, String email, String password, String dni, int age, String speciality, int experienceYears) {
         super(id, userType, email, password, dni, age);
