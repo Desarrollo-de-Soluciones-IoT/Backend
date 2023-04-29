72b61ba1adcdaa654196a9d6e2fa641b51402a08
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2023-04-29 13:46:50 -0500

    Add attributes

--- src/main/java/com/docseeker/backend/model/Patient.java
+++ src/main/java/com/docseeker/backend/model/Patient.java
@@ -1,14 +1,26 @@
 package com.docseeker.backend.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.util.Date;
 import java.util.List;
 
 @Entity
 @Table(name = "patients")
+@AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
 public class Patient extends User {
+    private int height;
+    private int weight;
+    private Date birthDate;
+    private String phoneNumber;
+
     @OneToMany(mappedBy = "patient", cascade = CascadeType.DETACH)
     private List<Appointment> appointments;
 }
