de28677d56baf9b80fc9c4ade03c641b4a9861af
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2023-05-18 11:36:33 -0500

    Create Review entity

---
+++ src/main/java/com/docseeker/backend/model/Review.java
@@ -0,0 +1,27 @@
+package com.docseeker.backend.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "review")
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(length = 1000)
+    private String description;
+    private int rating;
+    @ManyToOne
+    @JoinColumn(name = "doctor_id")
+    @JsonBackReference
+    private Doctor doctor;
+}
