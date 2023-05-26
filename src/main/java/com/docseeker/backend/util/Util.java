546309c5fed5779195a01e13b5a6066611711fde
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2023-05-25 21:03:19 -0500

    Add utility method to calculate a patient's BMI

---
+++ src/main/java/com/docseeker/backend/util/Util.java
@@ -0,0 +1,12 @@
+package com.docseeker.backend.util;
+
+public class Util {
+
+    private Util() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static float calculateBMI(float height, float weight) {
+        return (float) (weight / Math.pow(height / 100.0, 2));
+    }
+}
