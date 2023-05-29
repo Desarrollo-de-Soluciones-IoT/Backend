f59be43893566361726878a21e55f8c351a44634
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2023-05-29 01:37:24 -0500

    Add custom query

--- src/main/java/com/docseeker/backend/repository/AppointmentRepository.java
+++ src/main/java/com/docseeker/backend/repository/AppointmentRepository.java
@@ -1,7 +1,13 @@
 package com.docseeker.backend.repository;
 
 import com.docseeker.backend.model.Appointment;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface AppointmentRepository extends ListCrudRepository<Appointment, Integer> {
+    @Query("SELECT a, d FROM Appointment a LEFT JOIN Doctor d ON a.doctor_id = d.id WHERE d.id = :doctorId")
+    List<Object[]> customGetAppointmentsByDoctorId(@Param("doctorId") int doctorId);
 }
