77781ba45f1a4c792e2097de5094d74295e98653
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2023-05-30 00:51:32 -0500

    Create Medical History Record controller

---
+++ src/main/java/com/docseeker/backend/controller/MedicalHistoryRepositoryController.java
@@ -0,0 +1,39 @@
+package com.docseeker.backend.controller;
+
+import com.docseeker.backend.model.MedicalHistoryRecord;
+import com.docseeker.backend.repository.MedicalHistoryRecordRepository;
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/medical-history")
+public class MedicalHistoryRepositoryController {
+    private final MedicalHistoryRecordRepository repository;
+
+    public MedicalHistoryRepositoryController(MedicalHistoryRecordRepository repository) {
+        this.repository = repository;
+    }
+
+    @PostConstruct
+    private void init() {
+        MedicalHistoryRecord medicalHistoryRecord = new MedicalHistoryRecord();
+        medicalHistoryRecord.setAppointmentId(1);
+        medicalHistoryRecord.setDescription("Patient has a cold");
+        repository.save(medicalHistoryRecord);
+    }
+
+    @GetMapping("")
+    public List<MedicalHistoryRecord> findAll() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/patient/{patientId}")
+    public List<Object[]> getAppointmentsByPatientId(@PathVariable int patientId) {
+        return repository.getAppointmentsByPatientId(patientId);
+    }
+}
