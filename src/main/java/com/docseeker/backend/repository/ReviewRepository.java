3ae254ddb857c4c9013d0fe09d04d24032c9d31e
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2023-06-01 18:13:13 -0500

    Add function to return reviews by the doctor's id

--- src/main/java/com/docseeker/backend/repository/ReviewRepository.java
+++ src/main/java/com/docseeker/backend/repository/ReviewRepository.java
@@ -4,6 +4,9 @@ import com.docseeker.backend.model.Review;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ReviewRepository extends ListCrudRepository<Review, Integer> {
+    List<Review> findByAssociatedDoctorId(int id);
 }
