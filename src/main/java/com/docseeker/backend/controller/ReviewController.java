1e4b47fcd94b2c6b609834c3f168356a7fdcfed5
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2023-06-02 11:09:52 -0500

    Create function to create a Review

--- src/main/java/com/docseeker/backend/controller/ReviewController.java
+++ src/main/java/com/docseeker/backend/controller/ReviewController.java
@@ -8,10 +8,8 @@ import com.docseeker.backend.repository.DoctorRepository;
 import com.docseeker.backend.repository.PatientRepository;
 import com.docseeker.backend.repository.ReviewRepository;
 import jakarta.annotation.PostConstruct;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -83,5 +81,11 @@ public class ReviewController {
         }
 
         return reviewDTOs;
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("")
+    public void create(@RequestBody Review review) {
+        repository.save(review);
     }
 }
