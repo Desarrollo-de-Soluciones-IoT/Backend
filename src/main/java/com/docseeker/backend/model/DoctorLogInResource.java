6627536649e5699b1d915bec94bac92e35113b18
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2023-11-02 00:49:44 -0500

    Add doctor login resource

---
+++ src/main/java/com/docseeker/backend/model/DoctorLogInResource.java
@@ -0,0 +1,15 @@
+package com.docseeker.backend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class DoctorLogInResource {
+    private String dni;
+    private String password;
+}
